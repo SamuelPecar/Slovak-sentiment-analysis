1b6ca73065dce2df112198c3ea2bac98b9e40c70
Samuel Pecár <samuel.pecar@hotmail.com>
2019-07-22 12:18:10 +0200

    evaluation extracted

--- evaluate.py
+++ evaluate.py
@@ -14,7 +14,7 @@ from modules.trainers import ClassificationTrainer
 from modules.datasets.classification_dataset import ClassificationDataset
 
 from utils.model import RNNClassifier
-from utils.data import remap_data_to_2, remap_data_to_3, remap_data_to_3_from_text, get_histogram, save_predictions
+from utils.data import remap_data_to_2, remap_data_to_3, remap_data_to_3_from_text, save_predictions
 
 print('Loading dataset...')
 preprocessing = Preprocessing()
@@ -29,19 +29,8 @@ print(get_histogram_data(data[:, 2]))
 train_data, valid_data, test_data = split_data(data)
 x_column, y_column = data_params['x_column'], data_params['y_column']
 
-train_set = ClassificationDataset(train_data[:, x_column], train_data[:, y_column], preprocessing=preprocessing.process_text)
-valid_set = ClassificationDataset(valid_data[:, x_column], valid_data[:, y_column], preprocessing=preprocessing.process_text)
 test_set = ClassificationDataset(test_data[:, x_column], test_data[:, y_column], preprocessing=preprocessing.process_text)
-
-vocab = ConcatVocabulary([train_set.vocab, valid_set.vocab, test_set.vocab])
-
-train_loader = DataLoader(train_set, batch_size, shuffle=True, collate_fn=collate_fn_cf)
-valid_loader = DataLoader(valid_set, batch_size, shuffle=True, collate_fn=collate_fn_cf)
 test_loader = DataLoader(test_set, batch_size, collate_fn=collate_fn_cf)
-
-print(get_histogram_data(train_set.labels))
-print(get_histogram_data(valid_set.labels))
-print(get_histogram_data(test_set.labels))
 
 print('Creating model...')
 
@@ -61,18 +50,12 @@ best_macro_f1 = None
 gold_labels = test_set.labels.astype(int)
 
 for epoch in range(training_params['n_epochs']):
+    trainer.model = torch.load('checkpoints/best_valid_f1_model')
 
-    train_loss = trainer.train_model(train_loader)
-    valid_loss, predicted, model_predictions, labels = trainer.evaluate_model(valid_loader)
-
-    print('| Epoch: {} | Train Loss: {:2.5f} | Val. Loss: {:2.5f} | Val. Acc: {:2.5f} | Val. Macro F1: {:2.5f} | Val. Micro F1: {:2.5f} |'
-          .format(epoch + 1, train_loss, valid_loss, accuracy_score(labels, predicted),
-                  f1_score(labels, predicted, average='macro'), f1_score(labels, predicted, average='micro')))
-
-    macro_f1 = f1_score(labels, predicted, average='macro')
-
-    if not best_macro_f1 or macro_f1 > best_macro_f1:
-        print('saving...')
-        best_macro_f1 = macro_f1
-        torch.save(model, paths['f1_score']['model_path'])
-        save_predictions(name='submission' + str(epoch), predictions=predicted, original_data=test_data)
+    test_loss, predicted, model_predictions, labels = trainer.evaluate_model(test_loader)
+    print('----------------------------------------------------Test results----------------------------------------------------')
+    print('| Loss: {} | Acc: {}% |'.format(test_loss, accuracy_score(labels, predicted)))
+    print('| Macro Precision: {} | Micro Precision: {} |'.format(precision_score(gold_labels, predicted, average='macro'), precision_score(gold_labels, predicted, average='micro')))
+    print('| Macro Recall: {} | Micro Recall: {} |'.format(recall_score(gold_labels, predicted, average='macro'), recall_score(gold_labels, predicted, average='micro')))
+    print('| Macro F1: {} | Micro F1: {}|'.format(f1_score(gold_labels, predicted, average='macro'), f1_score(gold_labels, predicted, average='micro')))
+    print('--------------------------------------------------------------------------------------------------------------------')
